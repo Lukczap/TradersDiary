df54f20556414d61f12931b8225e16d9459239a7
Lukczap <lukczap@gmail.com>
2017-11-02 15:41:14 +0100

    Create calculating size position button

---
+++ app/src/main/java/tradersdiary/lukapplication/tradersdiary/MainActivity.java
@@ -0,0 +1,22 @@
+package tradersdiary.lukapplication.tradersdiary;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void positionSize(View view){
+
+        Intent intent = new Intent(MainActivity.this, PositionSize.class);
+        startActivity(intent);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/tradersdiary/lukapplication/tradersdiary/PositionSize.java
@@ -0,0 +1,40 @@
+package tradersdiary.lukapplication.tradersdiary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class PositionSize extends AppCompatActivity {
+
+    EditText accountSizeEditText;
+    EditText riskEditText;
+    EditText atrEditText;
+    EditText valueOfTickEditText;
+    Button  calculatingButton;
+
+
+    private void calculating(View view){
+
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.position_size);
+
+        accountSizeEditText = (EditText)findViewById(R.id.accountSizeEditText);
+        riskEditText = (EditText) findViewById(R.id.riskEditText);
+        atrEditText = (EditText)findViewById(R.id.atrEditText);
+        valueOfTickEditText = (EditText)findViewById(R.id.valueOfTickEditText);
+        calculatingButton = (Button)findViewById(R.id.calculatingButton);
+
+
+
+
+
+    }
+}
