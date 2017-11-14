9bb6cbfa65bff103effe13c05c2a6fc017b37f90
Lukczap <lukczap@gmail.com>
2017-11-14 14:00:59 +0100

    Create username and password field. and put login button, add new activity login2 and add new button logout.

--- app/src/main/java/tradersdiary/lukapplication/tradersdiary/MainActivity.java
+++ app/src/main/java/tradersdiary/lukapplication/tradersdiary/MainActivity.java
@@ -4,8 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button observationButton;
+    Button positionSizeButton;
+    Button positionNoteButton;
+    Button logoutButton;
 
     public void positionSize(View view){
 
@@ -18,5 +24,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        observationButton = (Button)findViewById(R.id.observationButton);
+        positionSizeButton = (Button)findViewById(R.id.positionSizeButton);
+        positionNoteButton = (Button)findViewById(R.id.positionNoteButton);
+        logoutButton = (Button)findViewById(R.id.logoutButton);
+
+
     }
 }
